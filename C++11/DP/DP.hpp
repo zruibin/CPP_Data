6951acb8a6634d7cd9ad93b17f0256ebdc3f8b9d
ruibin.chow <zhouruibin734@pingan.com.cn>
2020-08-17 13:32:54 +0800

    Design Pattern Env

---
+++ C++11/DP/DP.hpp
@@ -0,0 +1,28 @@
+/*
+ * DP.hpp
+ *
+ * Created by Ruibin.Chow on 2020/08/17.
+ * Copyright (c) 2020年 Ruibin.Chow All rights reserved.
+ */
+
+#ifndef DP_HPP
+#define DP_HPP
+
+#include <iostream>
+#include "Macros.h"
+
+
+NAMESPACE_START
+
+void testDP(void)
+{
+    std::cout<<'\n';
+    std::cout<<"---------------------------testDP start--------------------------------"<<std::endl;
+    
+    
+    std::cout<<"---------------------------testDP start--------------------------------"<<std::endl;
+}
+
+NAMESPACE_END
+
+#endif /* !DP_HPP */
